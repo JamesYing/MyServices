962098636e2f3298b0fbbf425c8bc4c18fe3ad81
jamesying <james@taogame.com>
2019-10-23 21:34:28 +0800

    初始化项目

---
+++ JCSoft.MyServices/Controllers/MyControllerBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JCSoft.MyServices.Controllers
+{
+    public class MyControllerBase : ControllerBase
+    {
+    }
+
+    [ApiController]
+    public abstract class MyApiControllerBase : MyControllerBase
+    {
+
+    }
+}
---
+++ JCSoft.MyServices/Controllers/WhoIsMeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JCSoft.MyServices.Controllers
+{
+    [Route("/api/who")]
+    public class WhoIsMeController : MyApiControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public WhoIsMeController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Content(_httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString());
+        }
+    }
+}
